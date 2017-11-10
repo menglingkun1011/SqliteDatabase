410261305a2814851c1edf4178b7b7341d7feb9c
MirsMeng <menglingkun_vip@126.com>
2017-11-10 19:30:39 +0800

    first提交

--- app/src/main/java/com/example/mirsmeng/slitedatabasedemo/db/MyOpenHelper.java
+++ app/src/main/java/com/example/mirsmeng/slitedatabasedemo/db/MyOpenHelper.java
@@ -14,15 +14,22 @@ public class MyOpenHelper extends SQLiteOpenHelper {
         super(context, "std.db", null, 1);
     }
 
+    //当数据库第一次创建的时候调用，适合做创建表操作，如果数据库文件已经创建  则不会调用
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("create table student(_id integer primary key autoincrement,name varchar(20),age integer)");
         db.execSQL("insert into student(name,age) values('张三',23)");
         db.execSQL("insert into student(name,age) values('李四',28)");
     }
-
+    //数据库升级是调用
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+    }
+
+    //降级操作  谷歌不推荐我们使用  如果使用 要把super注释掉
+    @Override
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+//        super.onDowngrade(db, oldVersion, newVersion);
     }
 }
